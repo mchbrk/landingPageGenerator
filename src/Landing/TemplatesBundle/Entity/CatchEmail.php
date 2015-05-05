2bb4e1d0189ef658ab454a28ebcf10a331c95406
michalwaluk <kontakt@createme.com.pl>
2015-05-05 10:22:54 +0200

    catching emails entity

---
+++ src/Landing/TemplatesBundle/Entity/CatchEmail.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Landing\TemplatesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * CatchEmail
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="Landing\TemplatesBundle\Entity\CatchEmailRepository")
+ */
+class CatchEmail
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var integer
+     *
+     * @ORM\ManyToOne(targetEntity="Landing\TemplatesBundle\Entity\SimpleLanding")
+     * @ORM\JoinColumn(name="template_id", referencedColumnName="id")
+     */
+    private $template;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="emails", type="string", length=255)
+     */
+    private $emails;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="timestamp", type="datetime")
+     */
+    private $timestamp;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set template
+     *
+     * @param integer $template
+     * @return CatchEmail
+     */
+    public function setTemplate($template)
+    {
+        $this->template = $template;
+
+        return $this;
+    }
+
+    /**
+     * Get template
+     *
+     * @return integer 
+     */
+    public function getTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * Set emails
+     *
+     * @param string $emails
+     * @return CatchEmail
+     */
+    public function setEmails($emails)
+    {
+        $this->emails = $emails;
+
+        return $this;
+    }
+
+    /**
+     * Get emails
+     *
+     * @return string 
+     */
+    public function getEmails()
+    {
+        return $this->emails;
+    }
+
+    /**
+     * Set timestamp
+     *
+     * @param string $timestamp
+     * @return CatchEmail
+     */
+    public function setTimestamp($timestamp)
+    {
+        $this->timestamp = $timestamp;
+
+        return $this;
+    }
+
+    /**
+     * Get timestamp
+     *
+     * @return datetime
+     */
+    public function getTimestamp()
+    {
+        return $this->timestamp;
+    }
+}
